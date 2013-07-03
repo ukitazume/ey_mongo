9c0fd0b4c849e47a210a9bb7263ea77d14da6a24
Yu Kitazume <u.kitazume@gmail.com>
2013-07-03 18:06:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyMongo::Application.config.secret_token = '083420671fa698d80e9b65bdce49754a42826ad2390625fb7fb0b0089be5cd51c2370d9b6a6d9cd4243100af3f788c1192f6ef9fb4823e6f67e5ab5b8e89919c'
